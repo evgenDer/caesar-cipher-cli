e777264d44f3ac68a0abdb697c6a23ffa4ac48b6
Evgeniya <evgeniya375447249020@gmail.com>
2020-10-02 00:52:50 +0300

    fix: validation action

---
+++ caesar-cipher-cli/src/validation/validation.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function writeErrorStream(errorMsg) {
+  process.stderr.write(errorMsg);
+  process.exit(3);
+}
+
+function validationOptions({ shift, action, input, output }) {
+  if (!action) {
+    writeErrorStream('Action is required');
+  }
+
+  if (!shift) {
+    writeErrorStream('Shift is required');
+  }
+
+  if (action !== 'encode' && action !== 'decode') {
+    writeErrorStream('Wrong action name. Encode or decode is alowable');
+  }
+
+  if (!Number.isInteger(+shift) || +shift < 0) {
+    writeErrorStream('Shift must be positive integer');
+  }
+
+  if (input && !fs.existsSync(input)) {
+    writeErrorStream('Input file was not found');
+  }
+
+  if (output && !fs.existsSync(output)) {
+    writeErrorStream('Output file was not found');
+  }
+}
+
+module.exports = validationOptions;
